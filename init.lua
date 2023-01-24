1f58e11f40879e46cc14f537aca95f6d19563647
Sirrobzeroone <44497598+sirrobzeroone@users.noreply.github.com>
2023-01-24 20:17:12 +1100

    Minetest 5.6
    Checked and working
    added bakedclay to mod.conf

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@
 --            \/             \/    \/              \/                 \/     \/    --
 --                                                                                 --
 --                  Orginally written/created by Pithydon/Pithy                    --
---					           Version 5.5.0.1                                     --
+--					           Version 5.6.0.1                                     --
 --                first 3 numbers version of minetest created for,                 --
 --                   last digit mod version for MT version                         --
 -------------------------------------------------------------------------------------
